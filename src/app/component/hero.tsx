041b364ad15938d165ad2b7554cb4b7d72c09acb
Ahmar06 <ahmarali2567@gmail.com>
2025-02-04 22:20:38 +0500

    changes

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Typewriter from "typewriter-effect";
-import { CiInstagram } from "react-icons/ci";
 
 const Hero = () => {
   return (
